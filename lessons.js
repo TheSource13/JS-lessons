94bd56cc110812b1d1152caf93df12cd36fe2cc8
Геннадий Кучеренко <gen_k_777@mail.ru>
2018-12-19 14:56:14 +0200

    Update lessons.js

--- lessons.js
+++ lessons.js
@@ -126,146 +126,343 @@ else
     
     }
 
-    function my_function(n) {
-      if (n<=1) return n;
-       else
-      return my_function(n-1)+" "+n;
+
+
+    // lesson 4
+
+    //  function declaration
+
+    myFirstFunction();
+    myFirstFunction();
+    myFirstFunction();
+    myFirstFunction();
+    myFirstFunction();
+
+    function myFirstFunction(){
+
+      console.log('Hello World');
+
+    } 
+
+    myFirstFunction();
+
+
+    function myFirstFunction(name, place){
+          
+      console.log('Hello ' + place +' My name is ' + name);
+    }
+
+    myFirstFunction('Nick', 'World');
+
+
+    function myFirstFunction(name, place){
+          
+      console.log('Hello ' + place +' My name is ' + name);
+    }
+
+    myFirstFunction();
+
+    function myFirstFunction(name, place){
+          
+      console.log('Hello ' + place +' My name is ' + name);
+    }
+
+    myFirstFunction('Nick');
+
+
+   var phrase = myFirstFunction('Nick' , 'World');
+      console.log(phrase);
+
+    function myFirstFunction(name, place){
+          
+    return('Hello ' + place +' My name is ' + name);
+    }
+
+
+    var phrase = myFirstFunction('Nick' , 'World');
+      console.log(phrase);
+
+    function myFirstFunction(name, place){
+          
+    ('Hello ' + place +' My name is ' + name);
+    }
+
+
+
+
+    var k = 'This is global function';
+    var phrase = myFirstFunction('Nick' , 'World');
+    console.log(phrase);
+     console.log(k);
+
+     function myFirstFunction(name, place){
+         k = 'This is local function';
+         
+        console.log(k);
+       return('Hello ' + place +' My name is ' + name);
+  }
+
+
+  // fn.expression
+  var  functionExpressoin= function(name){
+           return('Hello ' + name);
+
+  }
+
+  console.log(functionExpressoin('Nick'));
+    
+
+  
+  var  functionExpressoin= function(name){
+    return('Hello ' + name);
+
+}
+var rezult = functionExpressoin('Nick');
+
+// function parameters
+
+
+// рекрсия
+
+function forFn(max){
+  if(max < 0){
+    return
+  };
+console.log(max);
+forFn(max - 1);
+
+}
+
+forFn(10);
+
+
+for(var i = 0 ;  i < 10 ;  i++);
+console.log(i);
+
+function forFn(max){
+  if(max > 10){
+    return
+  };
+  if(max < 0){
+    return
+  };
+console.log(max);
+forFn(max + 1);
+
+}
+
+forFn(1);
+// 2 решение
+
+function forFn(max, start = 0){
+  if(start === max){
+return
+};
+  console.log(start);
+  forFn(max, start + 1);
+
+}
+forFn(10);
+
+// факториал
+
+function factorial(n){
+  if (n < 0){
+    return 'invalid Pararmetr';
+  }
+  var factorial = 1;
+
+    for(var i = 1; i <= n ; i++){
+      factorial = factorial * i;  
+       }
+       return factorial
+    }
+console.log(factorial(5));
+
+
+function factorial(n){
+
+  if (n < 0) {
+    return 'invalid Pararmeter'
+  }
+
+   var factorial = 1;
+    for(var i = 1; i<= n; i++) {
+      factorial = factorial * i;
+    }
+    return factorial
+
+}
+
+console.log(factorial(5));
+
+
+
+// Коля решает
+
+function factorial(n){
+  
+  if (n === 0){
+     return 1;
+}
+    return n * factorial(n-1);
+  
+   
+
+}
+
+console.log(factorial(5));
+
+
+function factorial(n){
+  if(n !== "number"){
+    return "Bad data";
+  }
+   
+  if (n === 0){
+     return 1;
+}
+    return n * factorial(n-1);
+  
+   
+
+} 
+
+console.log(factorial(5));
+
+
+
+
+function getNumLine(n)
+{
+    if (n == 0) return n;
+    else 
+    
+       return getNumLine(n - 1) + "," + n;
+}
+
+
+console.log(getNumLine(5));
+
+// Задание 1. Функция принимает число n(проверить что n является числом в противном случае завершить работу функции).
+//  Функция возвращает строку чисел от 1 до n через разделитель ','. 
+//  Если число меньше 1 то выдается строка (например n = -2 строка должна быть '1,0,-1,-2').
+
+/**
+ * Build string from numbers from 1 to n
+ * @param {number} input
+ * @returns {string} Returns the string from 1 to input.
+ */
+function stringBuilder(input) {
+    if(typeof input !=='number'){
+      return 'NaN';
     }
     
-    console.log(my_function(5)) ;
+    else
+      if(input >= 1){
+     return stringBuilder(input - 1) + ',';}
 
-    строки и регексп
+     else 
+       if(input < 1){ 
+        return stringBuilder(input - 1) + ',' + input;}
 
-    // string
+   
+};
 
-    let str = "Nick";
-
-
-    console.log('ma name is ' + str)
-
-
-    let str1 = "Nick";
-    let str2 = "Nick";
-    let str3 = "Nick";
-    let str4 =   "Nick";
-    let str5 = "Nick";
-
-    console.log('Our names is${str1} ${str2} ${str3} ${str4} ${str5}' )
+stringBuilder("l")
+module.exports = stringBuilder;
 
 
+console.log(typeof object)
 
-    let str1 = "Nick";
-    let str2 = "Nick";
-    let str3 = "Nick";
-    let str4 =   "Nick";
-    let str5 = "Nick";
-    let arrStr = "12.23? 34? 5.6.7.8.9.y, -= "
-    console.log('Hello.\n i\'m Nick')
+var i = 0;
+while (i < 3) {
+  console.log( i );
+  i++;
+}
 
+function my_function(n) {
+  if (n<=1) return n;
+   else
+  return my_function(n-1)+" "+n;
+}
+ var n = 6;
+console.log(my_function(6)) ;
 
-    console.log(str1. toUpperCase());
-  
-    console.log(str1. toLocaleLowerCase());
-
-    console.LOG(arrStr.slice(4.6))
-
+function getNumLine(n)
+{
+    if (n == 0) return n;
+    else 
     
-    console.log(arrStr.indexOf('23'));
-
-    console.log(arrStr.endsWith('y')
-
-    let pattern 
-
-    let regex = /abc/g;
-    console.log('abcdef'. match(regex));
+       return getNumLine(n - 1) + "," + n;
+}
 
 
-    let rudeWorld = 'fuck';
-    let rudeWorld = new RegExp('fuck', 'gi';)
-    let chat = 'fuck you fucking fuck'
-    console.log(chat.toLowerCase().replace(redeworld, 'honey'))
+getNumLine(5);
 
+function my_function(n) {
+  if (n<1) return n;
+   else
+  return  my_function(n-1)+ " " + n;
+}
 
-
-'gi' - игннор toLowerCase.toLowerCase
-
-
-let report = ' i pay 12 dollars for to  candies'
-let digit = /\d/g;
-console.log(report.replace(digit, '*'))
-
-
-let report = ' i pay 12 dollars for to  candies'
-let digit = /\d + dollars/g;
-console.log(report.replace(digit, '* dollars'));
+console.log(my_function(5)) ;
 
 
 
-let report = 'i    give   you   a monney'
-let digit = /\s+ /g;
-console.log(report.replace(digit," "));
+
+function my_function(n) { 
+   if (n>1)
+  return  my_function(n-1)+ " " + n;
+  else
+  if (n<1) return my_function(n+1) + ' ' + n;
+}
+
+console.log(my_function(-5)) ;
 
 
+function my_function(n) { 
+   if (n>=1)
+  return  my_function(n-1)+ " " + n;
+  else
+  if (n<1) return my_function(n+1) + ' ' + n;
+}
 
-let naming = /^my name is{\w}+.$/gi;
-let greet = ' my name is Nick.';
-console.log(greet.match(naming))
+console.log(my_function(-5)) ;
 
+// общий знаменатель
+const commonDevisor = ( num1, num2) => {
 
-var a = 100;
+ if (num2 == 0){
+   return num1;
+ }
+   return commonDevisor(num2, num1%num2);
 
-function fn(a)
-// var a = 10 ;
- console.log(typeof a);
-
-//  должно быть number
-
-//    14.12.18
-   var v= 10;
-   function myfn(){
-    var a = 12;
-
-     var kk =  function(){
-         var = 56;
-         kk()
-     }
    }
 
-   
-myfn();
-
-
-
-var fn = function(){
-    var k = 0;
-    return function(){
-
-        return cuonter +=1;
+   console.log(commonDevisor(100, 45));
+  
+  // без рекурсии
+   const gcd_2 = ( a, b) => {
+    var t;
+    while (b != 0) {
+      t = b;
+      b = a % b;
+      a = t;
     }
-}
-   
-const closure = fn();
-console.log
+    return a;
+  }
+
+  console.log(gcd_2(100,45));
 
 
+  const commonDevisor = (num1, num2) => {
+    if (!num2) {
+        return num1;
+    }
+    return commonDevisor(num2, num1 % num2);
+};
 
+console.log(commonDevisor(100,45));
+module.exports = commonDevisor;
 
-const arr = [1 .'apple', true, {}];
-arr.push('new element');      
-
-}
-
-
-
-var str = "1,2,3,"
-const arr1 = str.split(',');
-const arr2 = arr1.map(function(item){
-    return + item;
-});
-
-console.log(arr2)
-aar1.forEach(function(item, index){
-    console.log(Element ${item})
-});
-console.log(arr1);
-console.log(typeof arr1[0]);
