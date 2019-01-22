a72b725900ed43d5201ca31edc39b0ed12e046dd
Gena Kucherenko <gen_k_777@mail.ru>
2019-01-22 22:55:08 +0200

    ggg

--- lessons.js
+++ lessons.js
@@ -466,3 +466,138 @@ const commonDevisor = ( num1, num2) => {
 console.log(commonDevisor(100,45));
 module.exports = commonDevisor;
 
+строки и регексп
+
+// string
+
+let str = "Nick";
+
+
+console.log('ma name is ' + str)
+
+
+let str1 = "Nick";
+let str2 = "Nick";
+let str3 = "Nick";
+let str4 =   "Nick";
+let str5 = "Nick";
+
+console.log('Our names is${str1} ${str2} ${str3} ${str4} ${str5}' )
+
+
+
+let str1 = "Nick";
+let str2 = "Nick";
+let str3 = "Nick";
+let str4 =   "Nick";
+let str5 = "Nick";
+let arrStr = "12.23? 34? 5.6.7.8.9.y, -= "
+console.log('Hello.\n i\'m Nick')
+
+
+console.log(str1. toUpperCase());
+
+console.log(str1. toLocaleLowerCase());
+
+console.LOG(arrStr.slice(4.6))
+
+
+console.log(arrStr.indexOf('23'));
+
+console.log(arrStr.endsWith('y')
+
+let pattern 
+
+let regex = /abc/g;
+console.log('abcdef'. match(regex));
+
+
+let rudeWorld = 'fuck';
+let rudeWorld = new RegExp('fuck', 'gi';)
+let chat = 'fuck you fucking fuck'
+console.log(chat.toLowerCase().replace(redeworld, 'honey'))
+
+
+
+'gi' - игннор toLowerCase.toLowerCase
+
+
+let report = ' i pay 12 dollars for to  candies'
+let digit = /\d/g;
+console.log(report.replace(digit, '*'))
+
+
+let report = ' i pay 12 dollars for to  candies'
+let digit = /\d + dollars/g;
+console.log(report.replace(digit, '* dollars'));
+
+
+
+let report = 'i    give   you   a monney'
+let digit = /\s+ /g;
+console.log(report.replace(digit," "));
+
+
+
+let naming = /^my name is{\w}+.$/gi;
+let greet = ' my name is Nick.';
+console.log(greet.match(naming))
+
+
+var a = 100;
+
+function fn(a)
+// var a = 10 ;
+console.log(typeof a);
+
+//  должно быть number
+
+//    14.12.18
+var v= 10;
+function myfn(){
+var a = 12;
+
+ var kk =  function(){
+     var = 56;
+     kk()
+ }
+}
+
+
+myfn();
+
+
+
+var fn = function(){
+var k = 0;
+return function(){
+
+    return cuonter +=1;
+}
+}
+
+const closure = fn();
+console.log
+
+
+
+
+const arr = [1 .'apple', true, {}];
+arr.push('new element');      
+
+}
+
+
+
+var str = "1,2,3,"
+const arr1 = str.split(',');
+const arr2 = arr1.map(function(item){
+return + item;
+});
+
+console.log(arr2)
+aar1.forEach(function(item, index){
+console.log(Element ${item})
+});
+console.log(arr1);
+console.log(typeof arr1[0]);
