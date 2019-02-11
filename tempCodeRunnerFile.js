7938c88122cb745cb420ca502b93a046e4696b8a
Gena Kucherenko <gen_k_777@mail.ru>
2019-02-11 18:55:20 +0200

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-
-var arr = [[1, 2, 3,  4], [4, 5, 6], [7, 8, 9]];
-for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr[i].length; j++) {
-        console.log(arr[i][j]);
-    }
-
-}
